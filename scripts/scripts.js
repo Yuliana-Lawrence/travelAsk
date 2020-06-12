0188403dbcb995bc1f2f78b5eec087cf5f1b9ff5
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-12 11:03:45 -0400

    Added event handler on submit event + updated DOM dynamically

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
 // Namespace 
 const travelApp = {};
 travelApp.apiKey = 'apikeyhere';
+travelApp.countryName = $('.country');
+travelApp.capitalName = $('.capital');
+travelApp.language = $('.language');
+travelApp.currency = $('.currency');
+travelApp.populationNumb = $('.population');
+travelApp.countryInfo = $('.countryInfo');
 
 // Listener for when user clicks Start button on modal, fadeout the modal
 travelApp.addStartButton = function() {
@@ -24,8 +30,28 @@ travelApp.addFooterListener = function() {
 }
 
 // When user changes drop down and submits
+travelApp.addFormListener = function (){
+    $('form').on('submit', (e) => {
+        e.preventDefault();
 
-// Take the value from the drop down, store it and use it to call getCountryData 
+        // Take the value from the drop down, store it and use it to call getCountryData 
+        const optionValue = $('select option:selected').val();
+        const returnedCountry = this.getCountryData(optionValue);
+        returnedCountry.then( result => {
+
+            // Used destructuring for easier access to the returned data
+            const {name, capital, languages, currencies, population} = result[0];
+            
+            // Used this keyword to reference travelApp object (because we use function expression not an arrow function this will be equal to whatever is to the left of the dot)
+            this.countryName.text(name);
+            this.capitalName.text(capital);
+            this.language.text(languages[0].name);
+            this.currency.text(currencies[0].code);
+            this.populationNumb.text(population);
+        })
+    })
+}
+
 
 // Take the value from the drop down, use to reference lat and long from object, store that in a variable and call getCityWeatherData 
 
@@ -108,6 +134,7 @@ travelApp.countryData.done(function(item){
     console.log(item);
 })
 
+
 // travelApp.getCountryWeather = function (lat, long) {
 //     const weatherPromise = $.ajax({
 //         url: `https://api.darksky.net/forecast/${travelApp.apiKey}/42.3601,-71.0589`,
@@ -123,9 +150,14 @@ travelApp.countryData.done(function(item){
 //     console.log(item);
 // })
 
+
+
 travelApp.init = function() {
-    travelApp.addStartButton();
-    travelApp.addFooterListener();
+    /* In this case we can utilize this keyword as we use function expression and 'this' 
+    will be pointing to whatever is to the left of the dot */
+    this.addStartButton();
+    this.addFooterListener();
+    this.addFormListener();
 }
 
 $(function() {
